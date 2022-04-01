163ef4ffd79669e7921cbce7137d58f9b215833c
aluno <aluno@LAB06M29>
2022-04-01 14:31:23 -0300

    Começo do upload de arquivo, e criacao do controller

---
+++ src/java/br/edu/ifsp/pep/controller/UploadController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author aluno
+ */
+@Named
+@RequestScoped
+public class UploadController {
+    
+}
