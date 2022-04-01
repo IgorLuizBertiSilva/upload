be6d8f5672de74a6015bcdaa76e1e82d3760c48d
aluno <aluno@LAB06M29>
2022-04-01 15:07:47 -0300

    Upload implematado completamente e classe util adiconada no projeto (ela oferece a opção de mandar os growls)

--- src/java/br/edu/ifsp/pep/controller/UploadController.java
+++ src/java/br/edu/ifsp/pep/controller/UploadController.java
@@ -4,6 +4,14 @@
  */
 package br.edu.ifsp.pep.controller;
 
+import br.edu.ifsp.pep.util.Util;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import org.primefaces.model.file.UploadedFile;
@@ -15,8 +23,11 @@ import org.primefaces.model.file.UploadedFile;
 @Named
 @RequestScoped
 public class UploadController {
+
+    private final String DIRETORIO = "/home/aluno/uploads/";
     
     private UploadedFile file;
+    
 
     public UploadedFile getFile() {
         return file;
@@ -26,9 +37,29 @@ public class UploadController {
         this.file = file;
     }
 
+    public void upload() {
+        if (file != null) {
+            System.out.println("Chegou arquivo: " + file.getFileName() + " e seu tama"
+                    + "nho é de: " + file.getSize() + "bitys");
+            
+            String nomeArquivo = UUID.randomUUID()+ " -- " + file.getFileName();
+            
+            Path arquivo = Paths.get(DIRETORIO + nomeArquivo);
+            
+            try {
+                Files.copy(file.getInputStream(), arquivo);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                Util.addMessageError(ex.getMessage());
+            }
+            
+            
+            
+            
+        }
+    }
+
     public UploadController() {
     }
-    
-    
-    
+
 }
---
+++ src/java/br/edu/ifsp/pep/util/Util.java
@@ -0,0 +1,25 @@
+package br.edu.ifsp.pep.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author aluno
+ */
+public class Util {
+    
+    public static void addMessageInformation(String message) {
+        FacesContext.getCurrentInstance().
+                addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação", message));
+    }
+    public static void addMessageWarning(String message) {
+        FacesContext.getCurrentInstance().
+                addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Atenção", message));
+    }
+    public static void addMessageError(String message) {
+        FacesContext.getCurrentInstance().
+                addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", message));
+    }
+    
+}
